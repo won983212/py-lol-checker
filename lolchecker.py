23d22c960dbbbe63da62e3c4046977d6808f2c2e
이현 <won983212@naver.com>
2021-09-14 09:40:13 +0900

    clean up code

--- lolchecker.py
+++ lolchecker.py
@@ -1,19 +1,24 @@
-from riotwatcher import LolWatcher, ApiError
+from riotwatcher import LolWatcher
 from requests.exceptions import HTTPError
 from datetime import datetime
 from pyfcm import FCMNotification
 import config
 import time
 
+
 print('Registering API Keys')
 lol_watcher = LolWatcher(config.RIOT_API_KEY)
 push_service = FCMNotification(config.FCM_API_KEY)
+time_phase = -1
 
 
-def to_timespan(delta):
-    delta = delta.total_seconds()
-    if delta > 60 * 60 * 5:
-        return '방금 전'
+def calculate_playing_time(start_timestamp):
+    now = datetime.now()
+    start_time = datetime.fromtimestamp(start_timestamp / 1000)
+    if start_timestamp == 0 or now < start_time:
+        return '00:00'
+
+    delta = (now - start_time).total_seconds()
 
     m = int(delta / 60)
     if m < 10:
@@ -31,8 +36,19 @@ def send_message(body, title):
     })
 
 
+def info(message):
+    print("Info[t=" + str(time_phase) + "]: " + message)
+
+
+def debug(message):
+    if config.USE_DEBUG_LOG:
+        print("Debug[t=" + str(time_phase) + "]: " + message)
+
+
 def run():
+    global time_phase
     playerIDs = []
+    print('Loading player hashes...')
     for name in config.TARGET_PLAYERS:
         try:
             playerIDs.append({
@@ -43,9 +59,8 @@ def run():
             print('Incorrect player name: ' + name)
             return
 
-    print('Start watching...')
+    print('Start watching')
     send_message('게임 추적을 시작합니다. \n추적 대상: ' + ", ".join(config.TARGET_PLAYERS), '롤 게임 추적기')
-    time_phase = -1
 
     while True:
         time_phase += 1
@@ -54,23 +69,25 @@ def run():
                 if not player['playing'] and time_phase % config.TIMES_OF_DETECT_PERIOD_ON_PLAYING != 0:
                     continue
                 try:
+                    debug("Check " + player['data']['name'])
                     spectator = lol_watcher.spectator.by_summoner('kr', player['data']['id'])
+                    debug(str(spectator))
                     if not player['playing']:
                         player['playing'] = True
-                        start_time = datetime.fromtimestamp(spectator['gameStartTime'] / 1000)
-                        print('[Debug] NOW: ' + datetime.now().strftime("%Y/%m/%d, %H:%M:%S"))
-                        print('[Debug] start: ' + start_time.strftime("%Y/%m/%d, %H:%M:%S"))
-                        message = '{}님이 게임중입니다! (게임 시간: {})'.format(player['data']['name'], to_timespan(datetime.now() - start_time))
-                        print(message)
+                        start_time = calculate_playing_time(spectator['gameStartTime'])
+                        message = '{}님이 게임중입니다! (게임 시간: {})'.format(player['data']['name'], start_time)
+                        info(message)
                         send_message(message, '롤 게임 추적기')
-                except HTTPError:
+                except HTTPError as error:
+                    debug(str(error))
                     if player['playing']:
                         player['playing'] = False
                         message = player['data']['name'] + '님 게임이 끝났습니다.'
-                        print(message)
+                        info(message)
                         send_message(message, '롤 게임 추적기')
         except BaseException as error:
             print('Unexpected exception: {}'.format(error))
         time.sleep(config.DETECT_PERIOD_SEC / config.TIMES_OF_DETECT_PERIOD_ON_PLAYING)
+
 
 run()
